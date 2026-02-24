62643c5048282ba4503547434c6f5edc3cf42369
dianamilendry-lang <dianamilendry@gmail.com>
2026-02-24 03:42:35 -0600

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import os
-import pandas as pd
 import streamlit as st
-import plotly.express as px
+
+st.set_page_config(page_title="Dashboard Ventas Empaque", layout="wide")
+
+st.title("🚀 Dashboard Ventas Empaque")
+st.success("La aplicación está funcionando correctamente.")
